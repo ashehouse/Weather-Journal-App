c86e9c42ab7103ec7e2f7ff541f2a2645e3607f8
Jody-Dixon <jodymdixon@gmail.com>
2020-05-24 15:13:49 -0400

    Added a package and dependencies file started client side server code

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ projectData = {};
 const express = require('express');
 // Start up an instance of app
 const app = express();
+/* Dependencies */
+const bodyParser = require('body-parser');
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -17,10 +18,32 @@ app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
+// Add Get Route 
+app.get('/all', (req, res) => {
+    res.send(projectData);
+})
+
+// Add Post Route
+app.post('/add', (req, res) => {
+    let data = req.body;
+    projectData.temperature = data.temperature;
+    projectData.date = data.date;
+    projectData.userResponse = data.userResponse;
+    res.send(projectData);
+})
+
+// Add Post Route for adding incoming data to projectData
+
+
+
+function addTemperature (req, res) {
+    data.push(req.body);
+}
 
 // Setup Server
-const port = 8000;
+const port = 8080;
 const server = app.listen(port, () => {
     console.log('server running');
     console.log(`running on localhost: ${port}`)
 });
+
