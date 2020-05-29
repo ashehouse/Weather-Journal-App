581a956f72d18611b65a5ab330ab2506096dc554
Jody-Dixon <jodymdixon@gmail.com>
2020-05-29 10:03:29 -0400

    Add Base URL and APi key variables to clinet js

--- website/app.js
+++ website/app.js
@@ -1,11 +1,14 @@
 /* Global Variables */
 
+let url = "api.openweathermap.org/data/2.5/weather?zip=";
+let apiKey = "&appid=ee95c3022f1d9a663f173d79e3606653";
+
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
  
-const postData = async (url = '', data = {}) => {
+const postData = async ('/localApi', data = {}) => {
     console.log(data);
     const response = await fetch(url, {
         method: 'POST',
